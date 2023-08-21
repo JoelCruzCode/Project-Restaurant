2a5cae2c4aac95354e8d9114f5dfc896e323c510
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-21 06:21:53 -0700

    update webpack configuration

--- src/loadPage.js
+++ src/loadPage.js
@@ -1,3 +1,6 @@
+import "./style.css";
+import headerBG from "./assets/images/Enchiladas.jpg";
+
 const loadPage = () => {
   const content = document.querySelector("#content");
   const element = document.createElement("div");
@@ -6,6 +9,10 @@ const loadPage = () => {
   element.classList.add("content2");
   element.textContent = "element";
   content.textContent = "Content";
+  content.classList.add("hello");
+  const headerImg = new Image();
+  headerImg.src = headerBG;
+  content.appendChild(headerImg);
 };
 
 export default loadPage;
