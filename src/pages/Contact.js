10d658d525b28cf3664440d3e757171ac4ea921e
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-22 06:50:31 -0700

    Render contact information

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,25 +1,23 @@
-import { createParagraph } from "./functions";
+import { createElement, createForm, appendChildren } from "./functions";
 
 const renderContact = (parent) => {
-  //   const contact = document.createElement("div");
-  //   const p = document.createElement("p");
-  //   p.textContent = `Zumaya's Mexican Cafe
-  //   7317 Greenleaf Avenue, Whittier, California 90602, United States
+  const contact = document.createElement("div");
+  const contactForm = createForm();
+  const title = createElement("p", "Zumaya's Mexican Cafe", "#contact-info");
+  const address = createElement(
+    "p",
+    "7317 Greenleaf Avenue, Whittier, California 90602, United States",
+    "#contact-address"
+  );
+  const phone = createElement("p", "(562 698 5442", "#contact-number");
+  const hours = createElement(
+    "p",
+    "Hours \n Monday - Tuesday: Closed \n Wednesday - Thursday: 11am - 8pm \n Friday: 11am - 9pm \n Saturday: 8:30am - 9pm \n Sunday: 8:30am - 7pm",
+    "#contact-hours"
+  );
 
-  //   (562) 698 5442
-
-  //   Hours
-  //   Monday - Tuesday: Closed
-
-  //   Wednesday - Thursday: 11am - 8pm
-
-  //   Friday: 11am - 9pm
-
-  //   Saturday: 8:30am - 9pm
-
-  //   Sunday: 8:30am - 7pm`;
-  contact.appendChild(p);
-  parent.appendChild(contact);
+  appendChildren(contact, [title, address, phone, hours]);
+  appendChildren(parent, [contact, contactForm]);
 };
 
 export default renderContact;
