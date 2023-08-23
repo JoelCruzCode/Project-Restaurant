5e7008c86c553e2270f5e740eff125038a77b183
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-23 05:42:29 -0700

    fix typo with "hours" element

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,12 @@
 import { createElement, createForm, appendChildren } from "./functions";
 
 const renderContact = (parent) => {
+  parent.textContent = ``;
+  const sectionTitle = createElement("p", "Visit us", ".section-title");
+
   const contact = document.createElement("div");
   const contactForm = createForm();
+
   const title = createElement("p", "Zumaya's Mexican Cafe", "#contact-info");
   const address = createElement(
     "p",
@@ -10,14 +14,18 @@ const renderContact = (parent) => {
     "#contact-address"
   );
   const phone = createElement("p", "(562 698 5442", "#contact-number");
-  const hours = createElement(
-    "p",
-    "Hours \n Monday - Tuesday: Closed \n Wednesday - Thursday: 11am - 8pm \n Friday: 11am - 9pm \n Saturday: 8:30am - 9pm \n Sunday: 8:30am - 7pm",
-    "#contact-hours"
+  const hours = [];
+  hours.push(createElement("p", "Hours", "#contact-hours"));
+  hours.push(createElement("p", "Monday - Tuesday: Closed", "#contact-hours"));
+  hours.push(
+    createElement("p", "Wednesday - Thursday: 11am - 8pm", "#contact-hours")
   );
+  hours.push(createElement("p", "friday: 11am - 9pm", "#contact-hours"));
+  hours.push(createElement("p", "Saturday: 8:30am - 9pm", "#contact-hours"));
+  hours.push(createElement("p", "Sunday: 8:30am - 7pm", "#contact-hours"));
 
-  appendChildren(contact, [title, address, phone, hours]);
-  appendChildren(parent, [contact, contactForm]);
+  appendChildren(contact, [contactForm, title, address, phone, ...hours]);
+  appendChildren(parent, [sectionTitle, contact]);
 };
 
 export default renderContact;
