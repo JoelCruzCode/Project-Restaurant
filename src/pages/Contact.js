d66b9ddebbe1bcec8f8b71146c068d055e42f7f5
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-25 07:45:51 -0700

    Wrap content in containers

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,20 +2,22 @@ import { createElement, createForm, appendChildren } from "./functions";
 
 const renderContact = (parent) => {
   parent.textContent = ``;
-  const sectionTitle = createElement("p", "Visit us", ".section-title");
+  const sectionTitle = createElement("h2", "Visit Us", ".section-title");
 
-  const contact = document.createElement("div");
+  const contactContainer = createElement("div", "", ".contact-container");
+  const formContainer = createElement("div", "", ".subcontainer");
+  const formTitle = createElement("h4", "Contact Us", ".contact-title");
   const contactForm = createForm();
-
-  const title = createElement("p", "Zumaya's Mexican Cafe", "#contact-info");
+  const infoContainer = createElement("div", "", ".subcontainer");
+  const title = createElement("h4", "Zumaya's Mexican Cafe", ".contact-title");
   const address = createElement(
     "p",
     "7317 Greenleaf Avenue, Whittier, California 90602, United States",
     "#contact-address"
   );
-  const phone = createElement("p", "(562 698 5442", "#contact-number");
+  const phone = createElement("h4", "(562 698 5442", "#contact-number");
   const hours = [];
-  hours.push(createElement("p", "Hours", "#contact-hours"));
+  hours.push(createElement("h4", "Hours", ".contact-title"));
   hours.push(createElement("p", "Monday - Tuesday: Closed", "#contact-hours"));
   hours.push(
     createElement("p", "Wednesday - Thursday: 11am - 8pm", "#contact-hours")
@@ -24,8 +26,10 @@ const renderContact = (parent) => {
   hours.push(createElement("p", "Saturday: 8:30am - 9pm", "#contact-hours"));
   hours.push(createElement("p", "Sunday: 8:30am - 7pm", "#contact-hours"));
 
-  appendChildren(contact, [contactForm, title, address, phone, ...hours]);
-  appendChildren(parent, [sectionTitle, contact]);
+  appendChildren(infoContainer, [title, address, phone, ...hours]);
+  appendChildren(formContainer, [formTitle, contactForm]);
+  appendChildren(contactContainer, [formContainer, infoContainer]);
+  appendChildren(parent, [sectionTitle, contactContainer]);
 };
 
 export default renderContact;
