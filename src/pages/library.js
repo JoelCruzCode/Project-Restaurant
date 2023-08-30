7af2becb4e40ec74f03c348cfbf85ffa701215b0
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-29 18:48:27 -0700

    add optional object structure methods

--- src/pages/library.js
+++ src/pages/library.js
@@ -11,20 +11,104 @@ const Menu = () => {
     myMenu.every((item) => item.getName() !== food.getName())
       ? myMenu.push(food)
       : console.log(`${food.name} already exists`);
-    // myMenu.forEach((item) => console.log(item.getName(), food.getName()));
   };
 
   function deleteFood(food) {
     myMenu.forEach((item, index) => {
       if (item.getName() === food) {
-        console.log(`removing ${item.getName()}`);
+        console.log(`deleting ${item.getName()}`);
         myMenu.splice(index, 1);
       }
     });
   }
+  /////////////////////////////////////////////
 
+  let quessadillaChorizo = food(
+    "Quesadilla Chorizo",
+    10,
+    "Lunch/Dinner",
+    "Jack cheese with spicy Mexican Sausage, topped with queso fresco",
+    "Appetizers"
+  );
+
+  let nachos = food(
+    "Topotitos",
+    11,
+    "Lunch/Dinner",
+    "Nachos topped with guacamole, refried beans, cheese and serrano chile. Add Carnitas or Asada for $4 more",
+    "Appetizers"
+  );
+
+  const newMenu = {
+    breakfast: {},
+    lunch: {
+      appetizers: [],
+      soupSalads: [],
+      tacoSopes: [],
+      burritos: [],
+      plates: [],
+      combinations: [],
+    },
+    drinks: {
+      softDrinks: [],
+      alcohol: [],
+    },
+  };
+
+  // I could reduce these 2 functions down to 1 by using dot notation but I would have
+  //to make both objects and change addfood function
+  //   const addAField = (parent, field) => {
+  //     parent.hasOwnProperty(field)
+  //       ? console.log(`${field} already exist`)
+  //       : (parent[field] = {});
+  //   };
+  const addField = (field) => {
+    newMenu.hasOwnProperty(field)
+      ? console.log(`${field} already exist`)
+      : (newMenu[field] = {});
+  };
+
+  const addSubfield = (field, sub) => {
+    if (newMenu[field]?.[sub]) {
+      console.log(`${sub} already exists in ${field}`);
+    } else if (newMenu[field]) {
+      newMenu[field][sub] = [];
+    }
+  };
+
+  //Should i use dot notation?
+  const addAFood = (category, sub, food) => {
+    // let arr = newMenu.lunch.appetizer;
+    const arr = newMenu[category]?.[sub];
+    if (arr) {
+      arr.every((obj) => obj !== food)
+        ? arr.push(food)
+        : console.log(`${food.getName()} already exist"`);
+    } else console.log(`Issue resolving path, are you sure the fields exist?`);
+  };
+
+  const delFood = (field, food) => {
+    if (field.every((obj) => obj !== food)) {
+      console.log(`${food.getName()} does not exist`);
+      return;
+    }
+    for (let i = 0; i < field.length; i++) {
+      if (field[i] === food) {
+        field.splice(i, 1);
+        console.log(`${food.getName()} deleted`);
+        break;
+      }
+    }
+  };
+  //   delFood(newMenu.lunch.appetizer, nachos);
+  //   delFood(newMenu.lunch.appetizer, quessadillaChorizo);
+  //   delFood(newMenu.lunch.appetizer, nachos);
+  //   //   addAFood("lunch", "appetizer", nachos);
+  //   console.log(newMenu.lunch.appetizer[0]);
+  //   console.log(newMenu.lunch.appetizer[1].getName());
+  //   addAFood("lunch", "appetizer", nachos);
   // should I learn how to make real accessor properties? getters & setters
-  return { getMenu, addFood, deleteFood };
+  return { getMenu, addFood, deleteFood, addAFood, newMenu };
 };
 
 const menu = Menu();
@@ -86,7 +170,25 @@ let items = [
   asadaBurrito,
 ];
 
-items.forEach((item) => menu.addFood(item));
+let appetizers = [guacamole, nachos, quessadillaChorizo];
 
-console.log(menu.getMenu());
+let soupSalads = [ceasarSalad, tortillaSoup];
+
+let burritos = [asadaBurrito];
+
+items.forEach((item) => menu.addFood(item));
+appetizers.forEach((item) => menu.addAFood("lunch", "appetizers", item));
+soupSalads.forEach((item) => menu.addAFood("lunch", "soupSalads", item));
+burritos.forEach((item) => menu.addAFood("lunch", "burritos", item));
+console.log(menu.newMenu);
+for (let [key, value] of Object.entries(menu.newMenu)) {
+  //   console.log(key, value);
+  for (let [keys, values] of Object.entries(value)) {
+    // console.log(keys);
+    // console.log(values);
+    // values.forEach((value) => console.log(value.getName()));
+  }
+}
+
+// console.log(menu.getMenu());
 export { menu };
