6e0e3f43b1b5c83dccd196ed7e7c7d1c3f5862ad
Joel Cruz <136981207+JoelCruzCode@users.noreply.github.com>
2023-08-30 19:37:18 -0700

    add function to render specified food sections

--- src/pages/menu.js
+++ src/pages/menu.js
@@ -2,51 +2,30 @@ import { createElement, appendChildren } from "./functions";
 import { menu } from "./library";
 const renderMenu = (parent) => {
   parent.textContent = ``;
+
   const menuElement = createElement("div", "", ".menu");
   menuElement.classList.add("container");
+
   const sectionTitle = createElement("h2", "Menus", ".section-title");
   sectionTitle.classList.add("container");
+
   const menuNav = createElement("div", "", ".menu-nav");
   menuNav.classList.add("container");
+
   const breakFastBtn = createElement("button", "Breakfast", ".menu-btn");
   const lunchBtn = createElement("button", "Lunch/Dinner", ".menu-btn");
   const drinksBtn = createElement("button", "Drinks", ".menu-btn");
+
   appendChildren(menuNav, [breakFastBtn, lunchBtn, drinksBtn]);
   appendChildren(parent, [sectionTitle, menuNav, menuElement]);
   parent.appendChild(menuElement);
-  // Will add logic to render a certain menu depending on time of day later
-  renderBreakfast(menuElement);
+  menuChoice();
 
-  function renderBreakfast(parent) {
-    const sectionTitle = createElement("h3", "Breakfast", ".subsection-title");
-    parent.textContent = ``;
-    const menuItems = [];
-    menuItems.push(
-      createMenuItem(
-        "Chilaquiles",
-        14,
-        "Choose either chili rojo, verde, mole, chipotle or ranchera. Topped \
-        with melted cheese and served with two eggs "
-      )
-    );
-
-    menuItems.push(
-      createMenuItem(
-        "Chorizo",
-        14,
-        "Spicy sausage, scrambled with eggs and papas. Served with\
-    home-made tortillas "
-      )
-    );
-
-    menuItems.push(
-      createMenuItem(
-        "Huevos Rancheros",
-        14,
-        " Eggs served upon lightly fried tortillas and topped with chili of your choice; rojo, verde, or ranchera salsa. "
-      )
-    );
-    appendChildren(parent, [sectionTitle, ...menuItems]);
+  function menuChoice() {
+    let time = new Date().getHours();
+    if (21 > time && time > 11) {
+      renderFoodSection(menuElement, menu, "lunch");
+    } else renderFoodSection(menuElement, menu, "breakfast");
   }
 
   function createMenuItem(title, price, info) {
@@ -55,17 +34,27 @@ const renderMenu = (parent) => {
     const itemTitle = createElement("h4", title, ".item-title");
     const itemPrice = createElement("h4", price, ".item-price");
 
-    const itemInfo = createElement("p", info, ".item-info");
     appendChildren(menuHeading, [itemTitle, itemPrice]);
-    appendChildren(menuItem, [menuHeading, itemInfo]);
+    menuItem.appendChild(menuHeading);
+
+    if (typeof info === "object") {
+      let itemInfo;
+      info.forEach((current) => {
+        itemInfo = createElement("p", current, ".item-info");
+        menuItem.appendChild(itemInfo);
+      });
+    } else {
+      const itemInfo = createElement("p", info, ".item-info");
+      menuItem.appendChild(itemInfo);
+    }
+
     return menuItem;
   }
-  //////////////////////////////
-  // used to render food from a library, should add getters and setters and remove direct accessor property
+
+  // Used to render the entire menu object
   function renderFood(parent, menu) {
     parent.textContent = ``;
     for (let [key, value] of Object.entries(menu)) {
-      let menuItems = [];
       const sectionTitle = createElement("h3", key, ".subsection-title");
       for (let [keys, values] of Object.entries(value)) {
         let subsectionTitle = createElement(
@@ -73,24 +62,42 @@ const renderMenu = (parent) => {
           `${keys}`,
           ".subsection-title"
         );
+        appendChildren(parent, [sectionTitle, subsectionTitle]);
         values.forEach((food) =>
-          subsectionTitle.appendChild(
+          parent.appendChild(
             createMenuItem(food.getName(), food.getPrice(), food.getInfo())
           )
         );
-        sectionTitle.appendChild(subsectionTitle);
       }
-      appendChildren(parent, [sectionTitle]);
+    }
+  }
+  function renderFoodSection(parent, menu, field) {
+    parent.textContent = ``;
+
+    const sectionTitle = createElement("h3", field, ".subsection-title");
+    parent.appendChild(sectionTitle);
+    for (let [keys, values] of Object.entries(menu.getMenu(field))) {
+      let subsectionTitle = createElement("h3", `${keys}`, ".subsection-title");
+      parent.appendChild(subsectionTitle);
+      values.forEach((food) =>
+        parent.appendChild(
+          createMenuItem(food.getName(), food.getPrice(), food.getInfo())
+        )
+      );
     }
   }
 
   // Event Listeners
   breakFastBtn.addEventListener("click", function () {
-    renderBreakfast(menuElement);
+    renderFoodSection(menuElement, menu, "breakfast");
   });
 
   lunchBtn.addEventListener("click", function () {
-    renderFood(menuElement, menu.newMenu);
+    renderFoodSection(menuElement, menu, "lunch");
+  });
+
+  drinksBtn.addEventListener("click", function () {
+    renderFoodSection(menuElement, menu, "Drinks");
   });
 };
 
